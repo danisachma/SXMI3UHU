24fbcbafa8ef769f669ff11b6a234e9f3e5f84cb
Daniel Sachsenmaier <daniel@Mac.fritz.box>
2025-07-06 15:57:17 +0200

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import ShowComments from './ShowComments';
 
@@ -9,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Comments</h1>
+  
       </header>
       <main className="App-body">
         <ShowComments comments={[]} />
